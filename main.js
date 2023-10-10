8e2ba0debba4056134d0ef9024ad5ee1fd5a3884
GilmarKrauczuk <124721427+GilmarKrauczuk@users.noreply.github.com>
2023-10-10 17:30:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos') .onclick=tocaSomAplausos;
+
+function tocaSomVaia(){
+    document.querySelector('#som_tecla_vaia').play();
+}
+document.querySelector('.tecla_vaia') .onclick=tocaSomVaia;
+
+function tocaSomRisada(){
+    document.querySelector('#som_tecla_risada').play();
+}
+document.querySelector('.tecla_risada') .onclick=tocaSomRisada;
+
+
+
